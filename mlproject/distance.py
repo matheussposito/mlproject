fb99f3a7f784d116c5221baef669506490f53c00
Matheus Oliveira <matheus_so@yahoo.com.br>
2022-05-22 17:06:22 -0300

    testing CI failing a test

--- mlproject/distance.py
+++ mlproject/distance.py
@@ -15,7 +15,7 @@ def haversine(lon1, lat1, lon2, lat2):
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
     c = 2 * asin(sqrt(a))
     r = 6371  # Radius of earth in kilometers. Use 3956 for miles
-    return c * r
+    return c * r *2
 
 
 if __name__ == "__main__":
